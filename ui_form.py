0ad69f1cf51c60ff2be72caee0365fdcde3cdd53
Jacobo Monroy <162770749+KillahBlitz@users.noreply.github.com>
2025-10-21 00:13:23 -0600

    changed design

--- ui_form.py
+++ ui_form.py
@@ -15,13 +15,23 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QSizePolicy, QWidget)
+from PySide6.QtWidgets import (QApplication, QPushButton, QRadioButton, QSizePolicy,
+    QToolButton, QWidget)
 
 class Ui_Widget(object):
     def setupUi(self, Widget):
         if not Widget.objectName():
             Widget.setObjectName(u"Widget")
         Widget.resize(800, 600)
+        self.toolButton = QToolButton(Widget)
+        self.toolButton.setObjectName(u"toolButton")
+        self.toolButton.setGeometry(QRect(250, 240, 23, 23))
+        self.pushButton = QPushButton(Widget)
+        self.pushButton.setObjectName(u"pushButton")
+        self.pushButton.setGeometry(QRect(280, 180, 80, 24))
+        self.radioButton = QRadioButton(Widget)
+        self.radioButton.setObjectName(u"radioButton")
+        self.radioButton.setGeometry(QRect(320, 350, 91, 22))
 
         self.retranslateUi(Widget)
 
@@ -30,5 +40,8 @@ class Ui_Widget(object):
 
     def retranslateUi(self, Widget):
         Widget.setWindowTitle(QCoreApplication.translate("Widget", u"Widget", None))
+        self.toolButton.setText(QCoreApplication.translate("Widget", u"...", None))
+        self.pushButton.setText(QCoreApplication.translate("Widget", u"PushButton", None))
+        self.radioButton.setText(QCoreApplication.translate("Widget", u"RadioButton", None))
     # retranslateUi
 
