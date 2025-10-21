315c8f367e2aadc46a1ae60ae80a1d2991e7f92a
Jacobo Monroy <162770749+KillahBlitz@users.noreply.github.com>
2025-10-21 00:36:27 -0600

    added new interface and select type file

--- ui_form.py
+++ ui_form.py
@@ -15,23 +15,36 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QPushButton, QRadioButton, QSizePolicy,
-    QToolButton, QWidget)
+from PySide6.QtWidgets import (QApplication, QFrame, QLabel, QPushButton,
+    QSizePolicy, QWidget)
 
 class Ui_Widget(object):
     def setupUi(self, Widget):
         if not Widget.objectName():
             Widget.setObjectName(u"Widget")
-        Widget.resize(800, 600)
-        self.toolButton = QToolButton(Widget)
-        self.toolButton.setObjectName(u"toolButton")
-        self.toolButton.setGeometry(QRect(250, 240, 23, 23))
-        self.pushButton = QPushButton(Widget)
-        self.pushButton.setObjectName(u"pushButton")
-        self.pushButton.setGeometry(QRect(280, 180, 80, 24))
-        self.radioButton = QRadioButton(Widget)
-        self.radioButton.setObjectName(u"radioButton")
-        self.radioButton.setGeometry(QRect(320, 350, 91, 22))
+        Widget.resize(1231, 600)
+        self.frame = QFrame(Widget)
+        self.frame.setObjectName(u"frame")
+        self.frame.setGeometry(QRect(390, 160, 451, 251))
+        self.frame.setFrameShape(QFrame.Shape.StyledPanel)
+        self.frame.setFrameShadow(QFrame.Shadow.Raised)
+        self.label = QLabel(self.frame)
+        self.label.setObjectName(u"label")
+        self.label.setGeometry(QRect(110, 30, 221, 20))
+        font = QFont()
+        font.setFamilies([u"Berlin Sans FB"])
+        font.setPointSize(12)
+        self.label.setFont(font)
+        self.btn_readexcel = QPushButton(self.frame)
+        self.btn_readexcel.setObjectName(u"btn_readexcel")
+        self.btn_readexcel.setGeometry(QRect(110, 100, 80, 24))
+        font1 = QFont()
+        font1.setBold(True)
+        self.btn_readexcel.setFont(font1)
+        self.btn_readjson = QPushButton(self.frame)
+        self.btn_readjson.setObjectName(u"btn_readjson")
+        self.btn_readjson.setGeometry(QRect(260, 100, 80, 24))
+        self.btn_readjson.setFont(font1)
 
         self.retranslateUi(Widget)
 
@@ -40,8 +53,8 @@ class Ui_Widget(object):
 
     def retranslateUi(self, Widget):
         Widget.setWindowTitle(QCoreApplication.translate("Widget", u"Widget", None))
-        self.toolButton.setText(QCoreApplication.translate("Widget", u"...", None))
-        self.pushButton.setText(QCoreApplication.translate("Widget", u"PushButton", None))
-        self.radioButton.setText(QCoreApplication.translate("Widget", u"RadioButton", None))
+        self.label.setText(QCoreApplication.translate("Widget", u"\u00bfEn que forrmato esta el archivo?", None))
+        self.btn_readexcel.setText(QCoreApplication.translate("Widget", u"EXCEL", None))
+        self.btn_readjson.setText(QCoreApplication.translate("Widget", u"JSON", None))
     # retranslateUi
 
--- widget.py
+++ widget.py
@@ -14,6 +14,23 @@ class Widget(QWidget):
         super().__init__(parent)
         self.ui = Ui_Widget()
         self.ui.setupUi(self)
+        #al presionar el boton oculta frame y todo su contenido
+        self.ui.btn_readexcel.clicked.connect(self.init_process('excel'))
+        self.ui.btn_readjson.clicked.connect(self.init_process('json'))
+
+    def init_process(self, file_type):
+        #oculta el frame
+        def process():
+            self.ui.frame.hide()
+            if file_type == 'excel':
+                print("Iniciando proceso para archivo EXCEL")
+            elif file_type == 'json':
+                print("Iniciando proceso para archivo JSON")
+                
+        return process
+    
+
+
 
 
 if __name__ == "__main__":
