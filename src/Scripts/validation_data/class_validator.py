80f59a90ec5f9930d7ce6d7bf37d68ef8339c5a5
Jacobo Monroy <162770749+KillahBlitz@users.noreply.github.com>
2025-10-25 17:18:56 -0600

    refactor code

--- src/Scripts/validation_data/class_validator.py
+++ src/Scripts/validation_data/class_validator.py
@@ -3,14 +3,11 @@ from models.class_history import History
 from models.class_consulation import Consulation
 
 class Validator:
-    #valida si las clases de un modelo cumplen con ciertos criterios
     patient_valid_count = 0
     consulation_valid_count = 0 
     history_valid_count = 0
 
-    #este metodo recibe un row del data frame de pacientes y valida sus datos de acuerdo al modelo de Patient
-    def validate_patient_data(self, row) -> bool:
-        #guarda en variables los datos del row
+    def validate_patient_data(self, row):
         try:
             name = str(row['Nombre del paciente'])
             age = int(row['Edad'])
@@ -18,7 +15,6 @@ class Validator:
             weight = float(row['Peso'])
             height = float(row['Altura'])
             total_consultations = float(row['Numero totales de registros'])
-            #inicializa la clase de pydantic Patient, si no cumple con el modelo, lanza una excepcion
             patient = Patient(
                 name=name,
                 age=age,
